80abc75ec571728137ed25a225811bf7090ef20d
evriyanaindrasaputra <eindrasap@gmail.com>
2020-07-10 17:28:20 +0700

    data API & nampilin di card sudah bisa menggunakan Vuex

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,44 +1,43 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="d-flex justify-content-between col-md-6 border offset-md-3">
-        <div class="p-1 mx-auto my-3">
-          <div class="infected">
-            <h3>Infected</h3>
-            <!-- <p>{{dataCov}}</p> -->
-            <div class="iCountUp">
-              <ICountUp :delay="delay" :endVal="dataCov.confirmed" :options="options" />
-            </div>
-            <p>date</p>
+  <div class="row">
+    <div class="d-flex justify-content-between col-md-6 border offset-md-3">
+      <div class="p-1 mx-auto my-3">
+        <div class="infected">
+          <h3>Infected</h3>
+          <!-- <p>{{dataCov}}</p> -->
+          <div class="iCountUp">
+            <ICountUp :delay="delay" :endVal="dataCov.confirmed.value" :options="options" />
           </div>
+          <p>date</p>
         </div>
-        <div class="p-1 mx-auto my-3">
-          <div class="recovered">
-            <h3>recovered</h3>
-            <!-- <p>{{dataCov.recovered}}</p> -->
-            <div class="iCountUp">
-              <ICountUp :delay="delay" :endVal="endVal" :options="options" />
-            </div>
-            <p>date</p>
+      </div>
+      <div class="p-1 mx-auto my-3">
+        <div class="recovered">
+          <h3>recovered</h3>
+          <!-- <p>{{dataCov.recovered}}</p> -->
+          <div class="iCountUp">
+            <ICountUp :delay="delay" :endVal="dataCov.recovered.value" :options="options" />
           </div>
+          <p>date</p>
         </div>
-        <div class="p-1 mx-auto my-3">
-          <div class="death">
-            <h3>death</h3>
-            <!-- <p>{{dataCov.deaths}}</p> -->
-            <div class="iCountUp">
-              <ICountUp :delay="delay" :endVal="endVal" :options="options" />
-            </div>
-            <p>date</p>
+      </div>
+      <div class="p-1 mx-auto my-3">
+        <div class="death">
+          <h3>death</h3>
+          <!-- <p>{{dataCov.deaths}}</p> -->
+          <div class="iCountUp">
+            <ICountUp :delay="delay" :endVal="dataCov.deaths.value" :options="options" />
           </div>
+          <p>date</p>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import axios from "axios";
+// import axios from "axios";
 import ICountUp from "vue-countup-v2";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Card",
@@ -47,6 +46,9 @@ export default {
   },
   data() {
     return {
+      confirmed: "",
+      recovered: "",
+      deaths: "",
       delay: 1000,
       endVal: 11641640,
       options: {
@@ -56,30 +58,17 @@ export default {
         decimal: ".",
         prefix: "",
         suffix: ""
-      },
-      dataCov: [
-        {
-          confirmed: "",
-          recovered: "",
-          deaths: ""
-        }
-      ]
+      }
     };
   },
   methods: {
-    async fetchData() {
-      await axios.get("https://covid19.mathdro.id/api").then(response => {
-        this.dataCov.confirmed = response.data.confirmed.value;
-        this.dataCov.recovered = response.data.recovered.value;
-        this.dataCov.deaths = response.data.deaths.value;
-        console.log(response.data);
-      });
-    }
-    // onReady(instance, CountUp) {
-    //     instance.update(this.endVal + 100);
+    ...mapActions(["getDataCovid"])
   },
   mounted() {
-    this.fetchData();
+    this.getDataCovid();
+  },
+  computed: {
+    ...mapGetters(["dataCov"])
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    dataCov: "",
+    //   confirmed: "",
+    //   recovered: "",
+    //   deaths: "",
+    // },
+  },
+
+  actions: {
+    async getDataCovid({ commit }) {
+      const res = await axios.get("https://covid19.mathdro.id/api");
+      commit("updateDataCovid", res.data);
+    },
+  },
+  mutations: {
+    updateDataCovid(state, dataCovid) {
+      state.dataCov = dataCovid;
+    },
+  },
+  getters: {
+    dataCov(state) {
+      return state.dataCov;
+    },
+  },
 });
